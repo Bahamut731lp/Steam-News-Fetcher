7cfdabdec59cfdd79eb8d88c35c9a37fd413e998
Bahamut731lp <bahamut731lp@gmail.com>
2018-08-02 15:08:51 +0200

    Fixed some weird behaviour

--- index.js
+++ index.js
@@ -7,28 +7,26 @@ window.onload = function(){
 }
 function update() {
 	let url = $("#webhooklink").prop("value");
-	if (url !== null || url !== undefined || url !== ''){
+	if (url == ''){
 		alert("Webhook link is empty!");
 	}
-	else{
-		games.forEach( (element) => {
-			$.get("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid="+element+"&count=3&maxlength=300&format=json", (data) => {
-				let description = data.appnews.newsitems[0].contents;
-				description = description.replace(/<[^>]*>/g, '');
-				localStorage.setItem("webhookURL", url);
-				$.post(url,
-					JSON.stringify({
-					"embeds": [
-						{
-							"title": data.appnews.newsitems[0].title,
-							"url": data.appnews.newsitems[0].url,
-							"description": description,
-							"color": 15158332
-						}]
-				})
-			);
-			});
+	games.forEach( (element) => {
+		$.get("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid="+element+"&count=3&maxlength=300&format=json", (data) => {
+			let description = data.appnews.newsitems[0].contents;
+			description = description.replace(/<[^>]*>/g, '');
+			localStorage.setItem("webhookURL", url);
+			$.post(url,
+				JSON.stringify({
+				"embeds": [
+					{
+						"title": data.appnews.newsitems[0].title,
+						"url": data.appnews.newsitems[0].url,
+						"description": description,
+						"color": 15158332
+					}]
+			})
+		);
 		});
-		alert('Successfully posted!');
-	}
+	});
+	alert('Successfully posted!');
 };
