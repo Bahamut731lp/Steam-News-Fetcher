3bec8e4cc9e8a941c11c126b6afb2384794a38a0
Bahamut731lp <bahamut731lp@gmail.com>
2018-08-02 14:44:33 +0200

    Error Checking

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-	games = ["413150", "578080", "271590"] //Stardew Valley, PUBG, GTAV
+	games = ["413150", "578080", "271590"] //Stardew Valley, PUBG, GTA:V
 	if (localStorage.getItem("webhookURL")){
 		console.log("Webhook has been found in localStorage");
 		$("#webhooklink").prop("value", localStorage.getItem("webhookURL"))
@@ -7,23 +7,28 @@ window.onload = function(){
 }
 function update() {
 	let url = $("#webhooklink").prop("value");
-	games.forEach( (element) => {
-		$.get("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid="+element+"&count=3&maxlength=300&format=json", (data) => {
-			let description = data.appnews.newsitems[0].contents;
-			description = description.replace(/<[^>]*>/g, '');
-			localStorage.setItem("webhookURL", url);
-			$.post(url,
-				JSON.stringify({
-				"embeds": [
-					{
-						"title": data.appnews.newsitems[0].title,
-						"url": data.appnews.newsitems[0].url,
-						"description": description,
-						"color": 15158332
-					}]
-			})
-		);
+	if (url !== null || url !== undefined || url !== ''){
+		alert("Webhook link is empty!");
+	}
+	else{
+		games.forEach( (element) => {
+			$.get("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid="+element+"&count=3&maxlength=300&format=json", (data) => {
+				let description = data.appnews.newsitems[0].contents;
+				description = description.replace(/<[^>]*>/g, '');
+				localStorage.setItem("webhookURL", url);
+				$.post(url,
+					JSON.stringify({
+					"embeds": [
+						{
+							"title": data.appnews.newsitems[0].title,
+							"url": data.appnews.newsitems[0].url,
+							"description": description,
+							"color": 15158332
+						}]
+				})
+			);
+			});
 		});
-	});
-	alert('Successfully posted!');
+		alert('Successfully posted!');
+	}
 };
